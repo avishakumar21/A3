e2f41824dea588b420494e5c43c9dbd75e358777
Avisha Kumar <ak754@cornell.edu>
2019-11-11 14:28:49 -0500

    testing default index

--- a_test.c
+++ a_test.c
@@ -88,3 +88,39 @@ void pool_exit(struct pool *pool, int level){
 		}
 	}
 }
+
+
+char *middle[] = {
+	"m0", "m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "m9", "m10", "m11", "m12", "m13", "m14", "m15", "m16", "m17", "m18", "m19"
+};
+char *high[] = {
+	"h0", "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "h11", "h12", "h13", "h14", "h15", "h16", "h17", "h18", "h19"
+};
+
+void swimmer(void *shared, void *arg){
+	struct pool *pool = (struct pool *) shared;
+	char *name = (char *) arg;
+	for (int i = 0; i < NEXPERIMENTS; i++) {
+		rthread_delay(random() % 10);
+		printf("swimmer %s entering pool\n", name);
+		pool_enter(pool, *name == 'h');
+		printf("swimmer %s entered pool\n", name);
+		rthread_delay(random() % 10);
+		printf("swimmer %s leaving pool\n", name);
+		pool_exit(pool, *name == 'h');
+		printf("swimmer %s left pool\n", name);
+	}
+}
+
+int main(){
+	struct pool pool;
+	pool_init(&pool);
+	for (int i = 0; i < NMIDDLE; i++) {
+		rthread_create(swimmer, &pool, middle[i]);
+	}
+	for (int i = 0; i < NHIGH; i++) {
+		rthread_create(swimmer, &pool, high[i]);
+	}
+	rthread_run();
+	return 0;
+}
--- b_test.c
+++ b_test.c
@@ -10,6 +10,7 @@
 #define NHIGH 7
 #define NEXPERIMENTS 5
 
+
 struct pool {
 	rthread_lock_t lock;
 	//an array of structs
@@ -38,7 +39,7 @@ void pool_init(struct pool *pool){
 }
 
 //set deafult index to an arbitary large number 
-int default_index = NMIDDLE*6000 + NHIGH*6000;
+int default_index = NMIDDLE*6 + NHIGH*6;
 void pool_enter(struct pool *pool, int level){
 	int index = default_index;
 	rthread_with(&pool->lock) {
@@ -131,4 +132,40 @@ void pool_exit(struct pool *pool, int level){
 			printf("level is not a valid parameter value (0 or 1)\n");			
 		}		
 	}
+}
+
+
+char *middle[] = {
+	"m0", "m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "m9"
+};
+char *high[] = {
+	"h0", "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9"
+};
+
+void swimmer(void *shared, void *arg){
+	struct pool *pool = (struct pool *) shared;
+	char *name = (char *) arg;
+	for (int i = 0; i < NEXPERIMENTS; i++) {
+		rthread_delay(random() % 10);
+		printf("swimmer %s entering pool\n", name);
+		pool_enter(pool, *name == 'h');
+		printf("swimmer %s entered pool\n", name);
+		rthread_delay(random() % 1000);
+		printf("swimmer %s leaving pool\n", name);
+		pool_exit(pool, *name == 'h');
+		printf("swimmer %s left pool\n", name);
+	}
+}
+
+int main(){
+	struct pool pool;
+	pool_init(&pool);
+	for (int i = 0; i < NMIDDLE; i++) {
+		rthread_create(swimmer, &pool, middle[i]);
+	}
+	for (int i = 0; i < NHIGH; i++) {
+		rthread_create(swimmer, &pool, high[i]);
+	}
+	rthread_run();
+	return 0;
 }
